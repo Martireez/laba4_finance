03e225d9d34b20fdbd7006d197b653c3ab9f1459
Martynov Artem <mr.artem2007martynov@yandex.ru>
2026-04-16 00:13:46 +0300

    feat: add AddOperationForm component

---
+++ src/data/categories.js
@@ -0,0 +1,9 @@
+export const categories = [
+  { id: 1, name: "Зарплата", type: "income" },
+  { id: 2, name: "Подработка", type: "income" },
+  { id: 3, name: "Еда", type: "expense" },
+  { id: 4, name: "Транспорт", type: "expense" },
+  { id: 5, name: "Развлечения", type: "expense" },
+  { id: 6, name: "Одежда", type: "expense" },
+  { id: 7, name: "Другое", type: "expense" }
+];
